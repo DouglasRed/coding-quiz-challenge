17fd297a21197325d2eba1ce05043c19803d000e
Doug Red <douglasred63@yahoo.com>
2022-06-18 16:04:37 -0500

    added basic layout of home page

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var timer = 0;
+var startQuizEl = document.querySelector("#start-quiz");
+
+var startQuiz = function () {
+  console.log("start Quiz");
+};
+
+startQuizEl.addEventListener("click", startQuiz);
